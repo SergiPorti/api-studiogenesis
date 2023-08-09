a9acf1410eceecae9d6bdc46a59e6d42e9d9f3ca
SergiPorti <serporterolo01@gmail.com>
2023-08-09 22:22:39 +0200

    Added attributes in User.php + added in migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'lastname',
+        'birthdate',
+        'username',
         'email',
         'password',
     ];
