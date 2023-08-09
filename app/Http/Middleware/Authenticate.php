c715a43214cee9e79e927e707d8792190dc0e257
SergiPorti <serporterolo01@gmail.com>
2023-08-09 22:23:39 +0200

    Little changes and little implementations

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -7,11 +7,12 @@ use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return $request->expectsJson() ? null : response()->json(['message' => 'No estàs autentificat']);
     }
 }
