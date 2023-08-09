b90a2d0eb1dacf1b4c27bb547ae1c26f4dbcd67c
SergiPorti <serporterolo01@gmail.com>
2023-08-09 22:22:19 +0200

    Added routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,10 +10,19 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| be assigned to the "api" middleware group. Make somethinPg great!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::post('/login', [AuthController::class, 'login'])->middleware('auth:sanctum');
+
+Route::middleware('api_auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/update-password', [AuthController::class, 'updatePassword']);
 });
+
+Route::post('/register', [AuthController::class, 'register']);
