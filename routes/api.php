5ccbcfe39113e709d5ea36d4afaa1cbd49cedc11
SergiPorti <serporterolo01@gmail.com>
2023-08-17 04:17:57 +0200

    Created new Routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,23 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-// Route::post('/login', [AuthController::class, 'login'])->middleware('auth:sanctum');
-
-Route::middleware(['auth:sanctum', 'optional_token'])->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [UserController::class, 'me']);
     Route::post('/update-password', [AuthController::class, 'updatePassword']);
-    Route::post('/loginToken', [AuthController::class, 'loginByToken']);
     Route::post('/logout', [AuthController::class, 'logout']);
-});
+    Route::post('/update', [UserController::class, 'update']);
 
+    Route::prefix('tickets')->group(function () {
+        Route::get('/', [TicketController::class, 'getTickets']);
+        Route::post('/create', [TicketController::class, 'create']);
+        Route::post('/update', [TicketController::class, 'update']);
+        Route::delete('/delete', [TicketController::class, 'delete']);
+        Route::get('/search', [TicketController::class, 'search']);
+    });
+});
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
